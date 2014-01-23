c8d4ed6b61783b124fe48754f123c8a4af3b9819
Michael Bauer <mihi@lo-res.org>
2014-01-23 09:59:33 +0100

    do something with the form..

--- feedbackform/views.py
+++ feedbackform/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
+from django.core.exceptions import ObjectDoesNotExist
 from feedbackform.models import Feedback, Event
 from feedbackform.forms import FeedbackForm
 
@@ -7,9 +8,24 @@ from feedbackform.forms import FeedbackForm
 
 def fbform(request):
     if request.method == "POST":
-        f = FeedbackForm(request.POST)
+        f = FeedbackForm(request.POST,instance=Feedback())
         if f.is_valid():
-            pass
+            f.save()
+            try:
+                e = Event.objects.get(
+                    name=request.POST['event'].strip())
+                if request.POST['email']:    
+                    r = e.badge_service.issue(
+                        e.badge,
+                        request.POST['email']
+                        )
+                    c = {"assertion": r['assertion'],
+                         "badge": r['badge'] }
+                else:
+                    c = {}
+            except ObjectDoesNotExist:
+                c = {}
+            return render_to_response("feedbackform/submitted.html",c)
         else:
             c = {"form": f}
     else:
