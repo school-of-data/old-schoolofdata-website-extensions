44dfc901ccf449d31dfa6a1996fe280be9fa9e11
Michael Bauer <mihi@lo-res.org>
2014-02-20 11:25:44 +0100

    moved static into scodaext

--- scodaext/settings.py
+++ scodaext/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'true') == 'true'
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 
 # Application definition
@@ -96,7 +96,7 @@ TEMPLATE_LOADERS = (
 
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'scodaext', 'static'),
     )
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
