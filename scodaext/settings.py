27ddeecb3e3a6143a7f5c03bd5d1464024400099
Michael Bauer <mihi@lo-res.org>
2014-03-28 11:00:45 +0100

    fix SITE_ID so it actually exists

--- scodaext/settings.py
+++ scodaext/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-SITE_ID = 0
+SITE_ID = 1
 
 
 # Quick-start development settings - unsuitable for production
