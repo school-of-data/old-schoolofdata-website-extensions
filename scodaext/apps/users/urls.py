b9e3cf153a27ab9d61da62968a12235e10055fec
Michael Bauer <mihi@lo-res.org>
2014-05-27 18:32:18 +0200

    added urls

---
+++ scodaext/apps/users/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+import scodaext.apps.users.views
+
+urlpatterns = patterns(
+    '',
+    url(r'^$',
+        'scodaext.apps.users.start'),
+    url(r'^new/$',
+        'scodaext.apps.users.views.new'),
+    url(r'^login/$',
+        'scodaext.apps.users.views.login'),
+    url(r'^logout/$',
+        'scodaext.apps.users.views.logout'),
+    url(r'^(?P<username>)/$',
+        'scodaext.apps.users.views.profile'),
+    url(r'^(?P<username>)/edit/$',
+        'scodaext.apps.users.views.editprofile')
+    )
