0bf2f5d957f9de05cd6379990d71f2d7df0a6201
Michael Bauer <mihi@lo-res.org>
2014-05-27 18:39:33 +0200

    added start view

--- scodaext/apps/users/views.py
+++ scodaext/apps/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
+from django.shortcuts import render_to_response, \
+    get_object_or_404
+from django.core.context_processors import csrf
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.template.context import RequestContext 
+
+from django.contrib.auth.models import User
+from scodaext.apps.user.models import *
 # Create your views here.
+
+def start(request):
+    return render_to_response("users/start.html",{},
+                               context_instance=RequestContext(request))
+
+
