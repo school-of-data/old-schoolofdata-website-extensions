4c1084724a486578aa052ecbbd595807d6448e2a
Michael Bauer <mihi@lo-res.org>
2014-05-29 11:11:51 +0200

    added profile form

---
+++ scodaext/apps/users/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+class EditProfileForm(forms.Form):
+    first_name = forms.CharField(label=_("Given Name(s)") max_length = 30, required = False)
+    last_name = forms.CharField(label=_("Family Name(s)") max_length = 30, required = False)
+    email = forms.EmailField(label=_("Email") max_length = 75, required = True)
+    description = forms.TextField(label=_("Description"))
+
+
