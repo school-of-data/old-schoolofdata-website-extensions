8cebd13607aee587e868bff57a3306fbfadb274a
Michael Bauer <mihi@lo-res.org>
2014-06-24 18:01:34 +0200

    made the markdown editor work

--- scodaext/static/js/slugname.js
+++ scodaext/static/js/slugname.js
@@ -1,12 +1,21 @@
 
-function slugify(s)
-    {
-        return s.toLowerCase().replace(/[^A-Za-z0-9-]/g,"-")
-        }
+var a=function() {
+    var slugify= function(s) 
+        {
+            return s.toLowerCase().replace(/[^A-Za-z0-9-]/g,"-")
+            }
+    
+    var slugifyName = function()
+        {
+            document.getElementsByName('slug')[0].value=slugify(this.value);
+            }
 
-function slugifyName()
-    {
-        document.getElementsByName('slug')[0].value=slugify(this.value);
+    document.getElementsByName('name')[0].onkeyup=slugifyName;
+    
+    if (Markdown) {
+        var converter = new Markdown.Converter();
+        var editor = new Markdown.Editor(converter);
+        editor.run();
         }
+    }();
 
-document.getElementsByName('name')[0].onkeyup=slugifyName;
