f90e7421352b942426b58bef5affe01c4cc11909
Michael Bauer <mihi@lo-res.org>
2014-06-25 13:41:40 +0200

    added api urls as infrastructure to support an api

--- scodaext/apiurls.py
+++ scodaext/apiurls.py
@@ -1,22 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-
-from django.contrib import admin
-#import scodaext.apps.simplequiz.urls
-#import scodaext.apps.feedbackform.urls
-import apiurls
-admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'scodaquiz.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^quiz/',include(scodaext.apps.simplequiz.urls)),
     # url(r'^feedback/',include(scodaext.apps.feedbackform.urls)),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/1/', include(apiurls)),
-    url(r'^', include('cms.urls'))
 )
 
 
